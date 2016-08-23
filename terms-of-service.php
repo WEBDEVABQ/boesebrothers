61b9413fb1edd27a3cd3e1c14de435395e373a81
Jamena McInteer <jamena@webdevabq.com>
2016-08-23 12:04:43 -0600

    Fixed terms of service and logo transparency

--- terms-of-service.php
+++ terms-of-service.php
@@ -9,7 +9,7 @@
 	<link rel="stylesheet" href="stylesheets/styles.css">
 	<link href='https://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
 </head>
-<body class="home">
+<body class="tos">
 <div class="content-container">
 	<header class="site-header">
 		<div class="wrap">
@@ -29,15 +29,17 @@
 	</header>
 <div class="container">
 	<div class="col-md-8 col-md-push-2">
-	<h1>Terms of Use</h1>
+		<div class="feature-content">
+			<h1>Terms of Use</h1>
 
-	By using this website, you agree to be bound by the following terms and agreements outlined in this website policy statement.  Additionally, by using this website, you certify that you are of legal drinking age.
-	<h3>COPYRIGHT</h3>
-	All content included on this website is the property of Boese Brothers Brewery or its content suppliers and protected by U.S. and international copyright law.  The content on this website may be used as an information source only.
-	<h3>USE OF WEBSITE</h3>
-	This website or any portion of this website may not be reproduced, duplicated, copied, sold, resold, or otherwise exploited for any commercial purpose that is not expressly permitted by the Boese Brothers Brewery.
-	<h3>DISCLAIMER</h3>
-	THIS WEBSITE IS PROVIDED BY Boese Brothers Brewery ON AN "AS IS" BASIS. Boese Brothers Brewery MAKES NO REPRESENTATIONS OR WARRANTIES OF ANY KIND, EXPRESS OR IMPLIED, AS TO THE OPERATION OF THE WEBSITE OR THE INFORMATION, CONTENT, MATERIALS, OR PRODUCTS INCLUDED ON THIS WEBSITE. TO THE FULL EXTENT PERMISSIBLE BY APPLICABLE LAW, Boese Brothers Brewery DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Boese Brothers Brewery WILL NOT BE LIABLE FOR ANY DAMAGES OF ANY KIND ARISING FROM THE USE OF THIS WEBSITE, INCLUDING, BUT NOT LIMITED TO DIRECT, INDIRECT, INCIDENTAL, PUNITIVE, AND CONSEQUENTIAL DAMAGES. APPLICABLE LAW.
+			By using this website, you agree to be bound by the following terms and agreements outlined in this website policy statement.  Additionally, by using this website, you certify that you are of legal drinking age.
+			<h3>COPYRIGHT</h3>
+			All content included on this website is the property of Boese Brothers Brewery or its content suppliers and protected by U.S. and international copyright law.  The content on this website may be used as an information source only.
+			<h3>USE OF WEBSITE</h3>
+			This website or any portion of this website may not be reproduced, duplicated, copied, sold, resold, or otherwise exploited for any commercial purpose that is not expressly permitted by the Boese Brothers Brewery.
+			<h3>DISCLAIMER</h3>
+			THIS WEBSITE IS PROVIDED BY Boese Brothers Brewery ON AN "AS IS" BASIS. Boese Brothers Brewery MAKES NO REPRESENTATIONS OR WARRANTIES OF ANY KIND, EXPRESS OR IMPLIED, AS TO THE OPERATION OF THE WEBSITE OR THE INFORMATION, CONTENT, MATERIALS, OR PRODUCTS INCLUDED ON THIS WEBSITE. TO THE FULL EXTENT PERMISSIBLE BY APPLICABLE LAW, Boese Brothers Brewery DISCLAIMS ALL WARRANTIES, EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Boese Brothers Brewery WILL NOT BE LIABLE FOR ANY DAMAGES OF ANY KIND ARISING FROM THE USE OF THIS WEBSITE, INCLUDING, BUT NOT LIMITED TO DIRECT, INDIRECT, INCIDENTAL, PUNITIVE, AND CONSEQUENTIAL DAMAGES. APPLICABLE LAW.
+		</div>
 	</div>
 </div>
 
